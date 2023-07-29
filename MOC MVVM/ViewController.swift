5f8d364d23cdc29511981d5c14a2e54e0943dc88
manukant tyagi <manukant.tyagi@senpiper.com>
2023-07-29 11:49:58 +0530

    Initial Commit

---
+++ MOC MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOC MVVM
+//
+//  Created by Manu on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
